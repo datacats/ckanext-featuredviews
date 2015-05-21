8997f7769d1a08eac32e64a820d628383de70b3d
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-05-21 12:35:46 -0400

    add js resources

--- ckanext/featuredviews/plugin.py
+++ ckanext/featuredviews/plugin.py
@@ -10,4 +10,4 @@ class FeaturedviewsPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'featuredviews')
+        toolkit.add_resource('fanstatic', 'featured')
