d92e2cf015ad920316265787b079ddd34b8e4fd9
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-05-21 14:28:27 -0400

    update action

--- ckanext/featuredviews/actions.py
+++ ckanext/featuredviews/actions.py
@@ -3,8 +3,9 @@ import logging
 
 from ckan.plugins.toolkit import get_validator, ValidationError
 from ckan.lib.dictization import table_dictize
-
 from ckan.logic import NotFound
+
+import ckan.lib.navl.dictization_functions as df
 
 log = logging.getLogger(__name__)
 
@@ -12,11 +13,11 @@ schema = {
     'resource_view_id': [get_validator('not_empty'), unicode],
     'package_id': [get_validator('ignore_empty'), unicode],
     'canonical': [get_validator('ignore_empty'), unicode],
-    'featured': [get_validator('ignore_empty'), unicode]
+    'homepage': [get_validator('ignore_empty'), unicode]
 }
 
 schema_get = {
-    'resource_view_id': [get_validator('not_emptry'), unicode]
+    'resource_view_id': [get_validator('not_empty'), unicode]
 }
 
 def featured_create(context, data_dict):
@@ -28,7 +29,7 @@ def featured_create(context, data_dict):
     featured = db.Featured()
     featured.resource_view_id = data['resource_view_id']
     featured.canonical = data['canonical']
-    featured.featured = data['featured']
+    featured.homepage = data['homepage']
     featured.save()
 
     session = context['session']
@@ -48,3 +49,21 @@ def featured_show(context, data_dict):
         raise NotFound()
 
     return table_dictize(featured, context)
+
+def featured_update(context, data_dict):
+    data, errors = df.validate(data_dict, schema, context)
+
+    if errors:
+        raise ValidationError(errors)
+
+    featured = db.Featured.get(resource_view_id=data['resource_view_id'])
+    if featured is None:
+        raise NotFound()
+
+    featured.canonical = data['canonical']
+    featured.homepage = data['homepage']
+    featured.save()
+
+    session = context['session']
+    session.add(featured)
+    session.commit()
--- ckanext/featuredviews/plugin.py
+++ ckanext/featuredviews/plugin.py
@@ -1,13 +1,22 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import actions
 
 
 class FeaturedviewsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IActions, inherit=True)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'featured')
+
+    def get_actions(self):
+        actions_dict = {
+            'featured_create': actions.featured_create,
+            'featured_show': actions.featured_show,
+            'featured_update': actions.featured_update
+        }
+        return actions_dict
