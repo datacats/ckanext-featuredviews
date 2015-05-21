3a9bcd409bcae5c33c6ba8340d89199f7f9710aa
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-05-21 19:27:55 -0400

    start on that javascript

--- ckanext/featuredviews/fanstatic/js/featured.js
+++ ckanext/featuredviews/fanstatic/js/featured.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     ckanapi = new CKAN.Client(location.protocol + "//" + location.host)
 
-    
+    var active_view = $('li.active.view_item:first').data('id')
+
+    $('input:checkbox.featured').change(function(){
+        if (this.checked) {
+            //ckanapi.action('featured_create')
+        }
+    });
 });
